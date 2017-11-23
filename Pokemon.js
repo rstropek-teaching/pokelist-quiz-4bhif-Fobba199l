99abd02f05c4c7182a34b8a98d23010c6a6a82ee
PeaceFighter <handy.lange@gmail.com>
2017-11-23 13:33:57 +0100

    Got a hint from a friend.
    Which made me to get started and at least a little bit is working now.

--- Pokemon.js
+++ Pokemon.js
@@ -1,17 +1,27 @@
 const pokemonList = document.getElementById('pokemons');
-var browserify = require('browserify');
-var b = browserify();
 
-(function() {
-fetch('https://pokeapi.co/api/v2/pokemon/').then(response => {
-  response.json().then(pokelist => {
-    let html = '';
-    for (const pokemon of pokelist.results) {
-      html += `<li>${pokemon.name}</li>`
-    }
+var offset = 0;
 
-    pokemonList.innerHTML = html;
+
+callPokemon();
+
+document.getElementById("next").onclick = nextListener();
+
+function callPokemon(){
+  fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset='+offset).then(response => {
+    response.json().then(pokelist => {
+      let html = '';
+      for (const pokemon of pokelist.results) {
+        html += `<li>${pokemon.name}</li>`
+      }
+  
+      pokemonList.innerHTML = html;
+      console.log(html);
+    });
   });
-});
-})();
-b.bundle().pipe(process.stdout);
+}
+
+function nextListener(){
+  offset = offset + 20;
+  callPokemon();
+}
